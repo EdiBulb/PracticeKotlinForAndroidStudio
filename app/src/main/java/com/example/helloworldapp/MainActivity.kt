95d8a9035cc9cc780c1a7cbaf15c72f763c008ac
Edibulb <gnsqud24@naver.com>
2024-09-14 17:23:18 +1200

    Initial commit

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.helloworldapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworldapp.ui.theme.HelloWorldAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val myButton: Button = findViewById(R.id.myButton)
+        val textView: TextView = findViewById(R.id.textView)
+
+        myButton.setOnClickListener {
+            textView.text = "Button Clicked!"
+        }
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldAppTheme {
+        Greeting("Android")
+    }
+}
